d9aaf0fc1f6ba50fc197edd2c458411ca283247d
Kamil Chmielewski <oplaner1@gmail.com>
2021-10-25 16:30:32 +0200

    Initial Commit

---
+++ Wspollokator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wspollokator
+//
+//  Created by Kamil Chmielewski on 25/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wspollokator/WspollokatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WspollokatorApp.swift
+//  Wspollokator
+//
+//  Created by Kamil Chmielewski on 25/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WspollokatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
