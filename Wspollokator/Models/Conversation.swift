b8e1b1964585dc2b2e7d64a0d1dcde5bfbba5164
Kamil Chmielewski <oplaner1@gmail.com>
2021-11-17 14:19:52 +0100

    Updated Conversation Model

--- Wspollokator/Models/Conversation.swift
+++ Wspollokator/Models/Conversation.swift
@@ -5,11 +5,8 @@
 //  Created by Piotr Czajkowski on 04/11/2021.
 //
 
-import Foundation
-
 struct Conversation: Identifiable {
-    let id = UUID()
-    
-    let participants: [User] // Should not contain ourselves.
-    let messages: [Message]
+    let id: Int
+    var participants: [User]
+    var messages: [Message]
 }
