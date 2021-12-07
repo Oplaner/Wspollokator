752dc42988ac56f24b012629446a47745b9013b3
Szymon Tamborski <szytamborski@gmail.com>
2021-12-07 12:31:01 +0100

    Fixes - Added LocationManager, manually setting the point of interest from MyProfile and  map for users searching

--- Wspollokator/Views/Maps/MapView.swift
+++ Wspollokator/Views/Maps/MapView.swift
@@ -9,42 +9,93 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @EnvironmentObject var viewModel: ViewModel
     
-    @State private var region = MKCoordinateRegion(
-            center: MapDetails.startingLocation,
-            span: MapDetails.defaultSpan)
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: MapDetails.startingLocation, span: MapDetails.defaultSpan)
     
-    let screen = UIScreen.main.bounds
+    @State private var allUsers: [User] = []
     
     // Temporary annotations placeholders
     let annotations = ViewModel.sampleUsers[0...2]
     
+    var searchedUsers: [User]?
+    
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $region,
-                interactionModes: .all,
-                showsUserLocation: true,
-                annotationItems: annotations) { user in
-                MapAnnotation(coordinate: user.pointOfInterest!) {
-                    ZStack {
-                        Circle()
-                            .strokeBorder(Color.black, lineWidth: 4)
-                            .background(Circle().foregroundColor(Appearance.buttonColor))
-                            .frame(width: 52, height: 52)
-                        Text("\(user.name)")
-                            .bold()
-                    }
-                    .foregroundColor(.white)
+            if searchedUsers == nil {
+                List {
+                    Text("Aby móc wyszukiwać współlokatorów, ustaw swój punkt")
+                        .foregroundColor(Appearance.textColor)
+                }
+            } else if searchedUsers!.isEmpty {
+                List {
+                    Text("Nie znaleziono osób spełniających zadane kryteria")
+                        .foregroundColor(Appearance.textColor)
+                }
+            } else {
+                Map(coordinateRegion: $region,
+                    interactionModes: .all,
+                    showsUserLocation: false,
+                    annotationItems: searchedUsers!) { user in
+                    mark(for: user)
                 }
             }
         }
     }
+    
+    private func mark(for user: User) -> some MapAnnotationProtocol {
+        if user == viewModel.currentUser {
+            return AnyMapAnnotationProtocol(MapAnnotation(coordinate: user.pointOfInterest!) {
+                ZStack {
+                    Circle()
+                        .strokeBorder(Color.black, lineWidth: 3)
+                        .background(Circle().foregroundColor(.white))
+                        .frame(width: 52, height: 52)
+                    Text("\("TY")")
+                        .foregroundColor(Appearance.textColor)
+                        .font(.caption)
+                        .bold()
+                }
+                .foregroundColor(.white)
+            })
+        } else {
+            return AnyMapAnnotationProtocol(MapAnnotation(coordinate: user.pointOfInterest!) {
+                NavigationLink {
+                    UserProfile(user: user)
+                } label: {
+                    Text("\(user.name)")
+                        .font(.caption)
+                        .bold()
+                        .padding(6)
+                        .foregroundColor(Color.white)
+                        .font(.body)
+                        .background(
+                            Circle()
+                                .strokeBorder(Color.black, lineWidth: 3)
+                                .background(Circle().foregroundColor(Appearance.buttonColor))
+                                .scaledToFill()
+                        )
+                }
+            })
+        }
+     }
+}
+
+private struct AnyMapAnnotationProtocol: MapAnnotationProtocol {
+  let _annotationData: _MapAnnotationData
+  let value: Any
+
+  init<WrappedType: MapAnnotationProtocol>(_ value: WrappedType) {
+    self.value = value
+    _annotationData = value._annotationData
+  }
 }
 
 enum MapDetails {
     static let startingLocation = CLLocationCoordinate2D(latitude: 52.237049, longitude: 21.01753)
-    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.12, longitudeDelta: 0.12)
     static let regionInMeters = CLLocationDistance(2000)
+    static let zoomSpan = MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
 }
 
 struct MapView_Previews: PreviewProvider {
--- Wspollokator/Views/Maps/MapViewContainer.swift
+++ Wspollokator/Views/Maps/MapViewContainer.swift
@@ -6,40 +6,86 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocationUI
 
 struct MapViewContainer: View {
+    @StateObject var mapData = LocationManager()
+    
+    @Environment(\.dismiss) var dismiss
     @State private var title = ""
-    @State private var subtitle = ""
+    @Binding var pointOfInterest: CLLocationCoordinate2D?
+    @State var tempCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            MapViewRepresentable(title: $title, subtitle: $subtitle)
+        ZStack {
+            MapViewRepresentable(mapData: mapData, title: $title, tempCoordinate: $tempCoordinate)
                 .ignoresSafeArea()
+                .environmentObject(mapData)
             
-            if !title.isEmpty {
-                HStack(spacing: 12) {
-                    Image(systemName: "info.circle.fill").font(.title).foregroundColor(.black)
+            VStack {
+                VStack {
+                    Text("Przytrzymaj palec na mapie, aby ustawić swój punkt")
+                        .font(.subheadline)
+                        .padding()
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(16)
                     
-                    VStack(alignment: .leading, spacing: 15) {
-                        Text(self.title)
-                            .font(.caption)
-                        Text(self.subtitle)
-                            .font(.caption)
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            mapData.centerViewOnUserLocation()
+                        }, label: {
+                            Image(systemName: "location.fill")
+                                .font(.title2)
+                                .padding()
+                                .background(.ultraThinMaterial)
+                                .clipShape(Circle())
+                                .foregroundColor(Color.gray)
+                        })
                     }
-                    .foregroundColor(Appearance.textColor)
-                    
                 }
-                .padding(12)
-                .background(Appearance.backgroundColor)
-                .cornerRadius(15)
+                
+                Spacer()
+                
+                if !title.isEmpty {
+                    VStack(spacing: 12) {
+                        
+                        HStack(spacing: 12) {
+                            Image(systemName:"info.circle.fill").font(.caption).foregroundColor(.black)
+                            
+                            VStack(alignment: .leading, spacing: 15) {
+                                Text(self.title)
+                                    .font(.subheadline)
+                            }
+                            .foregroundColor(Appearance.textColor)
+                            
+                        }
+                        
+                        Button {
+                            self.pointOfInterest = CLLocationCoordinate2D(latitude: tempCoordinate.latitude, longitude: tempCoordinate.longitude)
+                            mapData.centerMapOnLocation(coordinate: self.pointOfInterest!, mapView: mapData.mapView)
+                        } label: {
+                            Text("Ustaw na bieżącą lokalizację")
+                        }
+                    }
+                    .padding(12)
+                    .font(.subheadline)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(15)
+                }
             }
-            
+            .padding()
         }
+        .onAppear(perform: {
+            mapData.checkIfLocationServicesIsEnabled()
+        })
     }
 }
 
 struct MapViewContainer_Previews: PreviewProvider {
     static var previews: some View {
-        MapViewContainer()
+        MapViewContainer(pointOfInterest: .constant(CLLocationCoordinate2D(latitude: 52.237, longitude: 21.017)), tempCoordinate: CLLocationCoordinate2D(latitude: 52.237, longitude: 21.017))
     }
 }
